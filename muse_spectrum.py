bd25891aac1cbaadb6d6f4f454ac09329c7723e2
Roland Bacon <roland.bacon@univ-lyon1.fr>
2019-04-07 18:20:09 +0200

    Replace pyasl.airtovac2,vactoair2 (do not extend enough in IR) by mpdaf

--- muse_spectrum.py
+++ muse_spectrum.py
@@ -8,6 +8,7 @@ import sys
 
 import numpy as np
 from mpdaf.sdetect import Source
+from mpdaf.obj.spectrum import Spectrum
 
 import matplotlib.pyplot as plt
 from pyplatefit import __version__
@@ -28,11 +29,14 @@ pl = Platefit()
 data_dir = 'PLATEFIT_testdata/'
 name = 'udf_udf10_00010.fits'
 # name = 'udf_mosaic_01011_newmask.fits.gz'
-src = Source.from_file(data_dir+name)
+name = '/Users/rolandbacon/Dropbox/MUSE/GTO/UDF/DR2/orig_specs/ref00216.fits'
+sp = Spectrum(name)
+z = 0.76513
+#src = Source.from_file(name)
 
-sp = src.spectra[src.REFSPEC]
+#sp = src.spectra[src.REFSPEC]
 vdisp = 80.0
-z = src.z[src.z['Z_DESC']=='MUSE']['Z'][0]
+#z = src.z[src.z['Z_DESC']=='MUSE']['Z'][0]
 logger.debug('z = %f',z)
 
 cont = pl.contfit(sp, z, vdisp)
