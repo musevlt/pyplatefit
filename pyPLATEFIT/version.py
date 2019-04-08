8554292c03081b0c668f4fb89d705daa3859f1a6
Roland Bacon <roland.bacon@univ-lyon1.fr>
2019-04-08 05:01:01 +0200

    rm pyPLATEFIT directory

--- pyPLATEFIT/version.py
+++
@@ -1,6 +0,0 @@
-from pkg_resources import get_distribution, DistributionNotFound
-try:
-    __version__ = get_distribution('pyplatefit').version
-except DistributionNotFound:
-    # package is not installed
-    __version__ = None
