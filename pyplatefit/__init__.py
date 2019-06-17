8783cb7cca1580f4786ae9b255aa628797ccee5f
Simon Conseil <simon.conseil@univ-lyon1.fr>
2019-06-17 13:58:28 +0200

    Do not setup a logger for MPDAF

--- pyplatefit/__init__.py
+++ pyplatefit/__init__.py
@@ -7,7 +7,6 @@ def _setup_logging():
     import logging
     import sys
     from mpdaf.log import setup_logging
-    setup_logging('mpdaf', level=logging.INFO, stream=sys.stdout)
     setup_logging(__name__, level=logging.DEBUG, stream=sys.stdout)
 
 
