d3c25c9ec14de3137a703ee9c8fdd568eb0be38d
Roland Bacon <roland.bacon@univ-lyon1.fr>
2021-02-05 16:31:03 +0100

    add back fortran to setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,30 @@
+import os
 from setuptools import find_packages
-from numpy.distutils.core import setup
+from numpy.distutils.core import setup, Extension
 
-# Dependencies.
-install_requires = ['numpy', 'astropy', 'scipy', 'tqdm', 'joblib',
-                    'lmfit', 'mpdaf', 'numdifftools']
+# The Fortran extension for nnls_burst
+nnls_sources = ['mnbrak.f90', 'nnls_burst.f90', 'dbrent.f90',
+                'fit_cont_nnls.f90']
+ext = Extension('pyplatefit.nnls_burst',
+                [os.path.join('pyNNLS', f) for f in nnls_sources])
+
+# Dependencies. Limit version of ppxf for Python 2 users
+install_requires = ['numpy', 'astropy', 'scipy',
+                    'lmfit', 'mpdaf', 'emcee']
 
 setup(
     name='pyplatefit',
-    description='Fit emission/absorption lines in MUSE spectra',
-    author='Roland Bacon',
-    author_email='roland.bacon@univ-lyon1.fr',
+    description='TODO',
+    author='Madusha Gunawardhana',
+    author_email='gunawardhana@strw.leidenuniv.nl',
     use_scm_version=True,
     setup_requires=['setuptools_scm'],
     packages=find_packages(),
     package_data={
-        'pyplatefit': ['BC03/bc_models_subset_cb08_milesx__bursts_extlam.fit',
-                       'refdata/lines_table_platefit.fits']
+        'pyplatefit': ['BC03/bc_models_subset_cb08_miles_v1_bursts.fit']
     },
     zip_safe=False,
     include_package_data=True,
     install_requires=install_requires,
-    ext_modules=[],
+    ext_modules=[ext],
 )
